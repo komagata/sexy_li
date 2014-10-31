2c3ddb30abdc82a1b1fa70f4173f1e8f347420dd
Masaki Komagata <komagata@gmail.com>
2014-10-31 21:43:06 +0900

    Remove wasted spaces

--- lib/sexy_li/record_tag_helper.rb
+++ lib/sexy_li/record_tag_helper.rb
@@ -3,11 +3,11 @@ module SexyLi
     def li_for(records, prefix = nil, options = {}, &block)
       content_tag_for(:li, records, prefix, options, &block)
     end
-  
+
     def tr_for(records, prefix = nil, options = {}, &block)
       content_tag_for(:li, records, prefix, options, &block)
     end
-  
+
     def render_content_for(tag, records, prefix = nil, options = {})
       key = records.first.class.name.tableize.singularize
       partial = prefix && prefix[:partial] ? prefix[:partial] : key
@@ -15,11 +15,11 @@ module SexyLi
         render partial, key.to_sym => record
       end
     end
-  
+
     def render_li_for(records, prefix = nil, options = {})
       render_content_for(:li, records, prefix, options)
     end
-  
+
     def render_tr_for(records, prefix = nil, options = {})
       render_content_for(:tr, records, prefix, options)
     end
